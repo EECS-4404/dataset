0e50d90d75041363f62a9813e71ff542347c3978
Jaime Dantas <jaimejales@hotmail.com>
2020-12-09 23:44:39 -0300

    added dataset

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
 # Dataset
-The dataset selected is the $name. 
+The dataset selected is the [Electrical Grid Stability Simulated Data](http://archive.ics.uci.edu/ml/datasets/Electrical+Grid+Stability+Simulated+Data+#). 
+<div>
+<img src="/charts/bar_class" width="300px"</img> 
+</div>
 
-## Requirements
-- At least 500 data points
-- Numerical Features
+## Features after preprocessing
+- Binary classification (last column): 1 = stable | -1 = unstable
+- 7,240 data points (3,620 for each class)
+- 12 real inputs
+- Shuffled
+
+
+### Important notes
+- Since this dataset was imbalanced, we subsampled the class with fewer data points so each class has the same number of data points.
+- The last column of the dataset, i.e., the class, was replaced for 1 = stable | -1 = unstable
+- The last column of the inputs, i.e. i = 13, was removed since it is equal to the class and has no use in the analysis. 
+
 
 ## Structure
 ```
@@ -14,7 +26,12 @@ dataset
 │   └──dataset_training.txt
 │   
 └───testing
-    └──dataset_testing.txt
+│   └──dataset_testing.txt  
+│ 
+└───charts
+    └──bar_class.eps
+    └──bar_class.png
+
 ```
 
 
--- split_dataset.m
+++ split_dataset.m
@@ -1,18 +1,69 @@
 % Shuffle and split dataset
-x = dataset(:,1:end-1);  
-t = dataset(:,end);  
 
+% removes column stab
+dataset = [dataset(:,1:end-2) dataset(:,end)];
+
+% gets size
 [D,n] = size(dataset);
 
-% original dataset
-x = [x t];
-
 % shuffle the dataset
-random_data = dataset(randperm(D), :);
+dataset = dataset(randperm(D), :);
+
+% the class is imbalanced (has a 43% difference between the two classes)
+new_dataset = [];
+n_stable = 0;
+n_unstable = 0;
+
+% plot the class
+for i=1:D
+if(dataset(i,end)==1)
+n_stable = n_stable + 1;
+else
+n_unstable = n_unstable + 1;
+end
+end
+
+figure
+axis = {'Stable','Unstable'};
+bar_data = [n_stable, n_unstable]
+bar(bar_data);
+newXticklabel = {'Stable','Unstable'};
+set(gca,'XtickLabel',newXticklabel);
+xlabel('Class');
+ylabel('Samples')
+title('Class Distribution');
+print -depsc bar_class
+
+
+% puts stable in new dataset
+n_unstable = n_stable; % they will be the same
+new_D = n_stable + n_unstable; % this is the new D
+for i=1:D
+if(dataset(i,end)==1 && n_stable>0)
+new_dataset = [new_dataset; dataset(i,:)];
+n_stable = n_stable - 1;
+end
+end
+
+% puts instable in new dataset
+for i=1:D
+if(dataset(i,end)==-1 && n_unstable>0)
+new_dataset = [new_dataset; dataset(i,:)];
+n_unstable = n_unstable - 1;
+end
+end
+
+new_dataset = new_dataset(randperm(new_D), :);
+
+
 
 % training
-n_training = floor(D * 0.8);
-data_training = random_data(1:n_training,:);
+n_training = floor(new_D * 0.8);
+data_training = new_dataset(1:n_training,:);
 
-% testing 
-data_testing = random_data(n_training+1:end,:);
+% testing
+data_testing = new_dataset(n_training+1:end,:);
+
+% writes output
+dlmwrite('dataset_training.txt',data_training);
+dlmwrite('dataset_testing.txt',data_testing);
